39464f2925ee5144580f98e8140e5434a4d31d87
Evgeny Pogrebnyak <e.pogrebnyak@gmail.com>
2024-02-24 21:36:38 +0000

    fix: орфография

--- install.sh
+++ install.sh
@@ -1,4 +1,6 @@
-conda install -c conda-forge just -y
-pip install ghp-import
+# conda install -c conda-forge just -y
+# pip install ghp-import
 # a вот с установкой quarto помучаемся: https://quarto.org/docs/download/tarball.html
 # на Винде проще
+( echo ""; echo 'export PATH=$PATH:~/bin\n' ; echo "" ) >> ~/.profile
+source ~/.profile
